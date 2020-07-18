03579acfc6a4205d9fd40298cc86903e5a6a199d
tempname1533 <pedro_de_sousa@outlook.com>
2020-07-18 14:55:33 +0200

    Firebase Config

--- firebase/index.js
+++ firebase/index.js
@@ -2,13 +2,7 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBl1L9mOYaBksar_5sYaI1P7F2v_keUzzc",
-  authDomain: "fir-chat-4062f.firebaseapp.com",
-  databaseURL: "https://fir-chat-4062f.firebaseio.com",
-  projectId: "fir-chat-4062f",
-  storageBucket: "fir-chat-4062f.appspot.com",
-  messagingSenderId: "59300017379",
-  appId: "1:59300017379:web:5c58d60959836b8efdc1d6",
+  /* FIREBASE CONFIG */
 };
 
 firebase.initializeApp(firebaseConfig);
